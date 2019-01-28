e5b891c76495617ae04876828f020a1db9de46a0
neutronest <neutronest@gmail.com>
2019-01-28 23:39:32 +0800

    [@neutronest] Dev: lda dev

--- src/main/scala/com/neulab/Main.scala
+++ src/main/scala/com/neulab/Main.scala
@@ -1,5 +1,17 @@
 package com.neulab;
 
+import breeze.stats.distributions.{Dirichlet, Gaussian};
+import com.neulab.lda.LDA
+
 object Main extends App {
   println("Hello World!")
+
+  var gau = new Gaussian(1.0, 2.0)
+
+  for (i <- 1 to 100) {
+    println(gau.draw())
+  }
+
+  var ldaModel = new  LDA(0.1, 0.1, 10, 10, 100)
+  ldaModel.init()
 }
--- src/main/scala/com/neulab/lda/LDA.scala
+++ src/main/scala/com/neulab/lda/LDA.scala
@@ -3,13 +3,40 @@
   */
 package com.neulab.lda
 
+import breeze.linalg.{DenseMatrix, DenseVector}
+import breeze.stats
+import breeze.stats.distributions
+import breeze.stats.distributions.Dirichlet
+
 class LDA(
            var alpha: Double,
            var gamma: Double,
-           var n_topic: Int,
-           var n_documents: Int,
-           var n_vocab: Int){
+           var nTopic: Int,
+           var nDoucments: Int,
+           var nVocab: Int){
 
+  var z = DenseMatrix.zeros[Double](nDoucments, nVocab)
+  var documentTopicDistribution = DenseMatrix.zeros[Double](nDoucments, nTopic)
+  var topicWordDistribution = DenseMatrix.zeros[Double](nTopic, nVocab)
+
+  def init(): Unit = {
+
+    val r = scala.util.Random
+
+    z = z.map(x => r.nextInt(nTopic).toDouble)
+
+    documentTopicDistribution = documentTopicDistribution(breeze.linalg.*,::).map(
+      row => new distributions.Dirichlet(alpha * DenseVector.ones[Double](nTopic)).draw()
+    )
+
+    topicWordDistribution = topicWordDistribution(breeze.linalg.*, ::).map(
+      row => new distributions.Dirichlet(gamma * DenseVector.ones[Double](nVocab)).draw()
+    )
+  }
+
+  def train(documentTokenDenseMatrix: DenseMatrix[Double], iterTimes: Int=100) : Unit = {
+    print(z)
+  }
 
 
 }
