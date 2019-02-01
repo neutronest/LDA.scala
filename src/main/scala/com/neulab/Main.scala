a43e8ce5d25b95a7ae630c4b4a6346f21fa44326
neutronest <neutronest@gmail.com>
2019-02-01 23:41:33 +0800

    [@dongchao7] Dev: basic LDA

--- src/main/scala/com/neulab/Main.scala
+++ src/main/scala/com/neulab/Main.scala
@@ -1,17 +1,40 @@
+/**
+  *
+  */
 package com.neulab;
 
-import breeze.stats.distributions.{Dirichlet, Gaussian};
+import breeze.linalg.{DenseMatrix, DenseVector}
+import breeze.stats.distributions.{Dirichlet, Gaussian}
 import com.neulab.lda.LDA
 
 object Main extends App {
   println("Hello World!")
 
-  var gau = new Gaussian(1.0, 2.0)
+//  var gau = new Gaussian(1.0, 2.0)
+//
+//  for (i <- 1 to 100) {
+//    println(gau.draw())
+//  }
 
-  for (i <- 1 to 100) {
-    println(gau.draw())
-  }
 
-  var ldaModel = new  LDA(0.1, 0.1, 10, 10, 100)
+  val W = DenseVector(
+    0,1,2,3,4
+  )
+  val X = DenseMatrix(
+    (0,0,1,2,2),
+    (0,0,1,1,1),
+    (0,1,2,2,2),
+    (4,4,4,4,4),
+    (3,3,4,4,4),
+    (3,4,4,4,4)
+  )
+  val nDocuments = X.rows
+  val nVocab = W.length
+  val nTopic = 2
+
+
+  var ldaModel = new  LDA(1, 1, nTopic, nDocuments, nVocab)
   ldaModel.init()
+  ldaModel.train(X)
+  println(ldaModel.documentTopicDistribution)
 }
--- src/main/scala/com/neulab/lda/LDA.scala
+++ src/main/scala/com/neulab/lda/LDA.scala
@@ -3,10 +3,11 @@
   */
 package com.neulab.lda
 
-import breeze.linalg.{DenseMatrix, DenseVector}
+import breeze.linalg.{DenseMatrix, DenseVector, sum}
 import breeze.stats
 import breeze.stats.distributions
-import breeze.stats.distributions.Dirichlet
+import breeze.numerics.{exp, log, log1p}
+import breeze.stats.distributions.{Dirichlet, Multinomial}
 
 class LDA(
            var alpha: Double,
@@ -15,7 +16,7 @@ class LDA(
            var nDoucments: Int,
            var nVocab: Int){
 
-  var z = DenseMatrix.zeros[Double](nDoucments, nVocab)
+  var z = DenseMatrix.zeros[Int](nDoucments, nVocab)
   var documentTopicDistribution = DenseMatrix.zeros[Double](nDoucments, nTopic)
   var topicWordDistribution = DenseMatrix.zeros[Double](nTopic, nVocab)
 
@@ -23,7 +24,8 @@ class LDA(
 
     val r = scala.util.Random
 
-    z = z.map(x => r.nextInt(nTopic).toDouble)
+    z = z.map(x => r.nextInt(nTopic))
+
 
     documentTopicDistribution = documentTopicDistribution(breeze.linalg.*,::).map(
       row => new distributions.Dirichlet(alpha * DenseVector.ones[Double](nTopic)).draw()
@@ -34,8 +36,60 @@ class LDA(
     )
   }
 
-  def train(documentTokenDenseMatrix: DenseMatrix[Double], iterTimes: Int=100) : Unit = {
-    print(z)
+  def train(documentTokenDenseMatrix: DenseMatrix[Int], iterTimes: Int=1000) : Unit = {
+
+    val iterStart = 1
+    val idxStart = 0
+    for (step <- iterStart to iterTimes) {
+
+      for (documentIdx <- idxStart to nDoucments-1) {
+        for (wordIdx <- idxStart to nVocab-1) {
+//          println("documentIdx: ", documentIdx)
+//          println("wordIdx: ", wordIdx)
+//          println(log(documentTopicDistribution(documentIdx,::)))
+//          println(log(topicWordDistribution(::, documentTokenDenseMatrix(documentIdx, wordIdx)).t))
+
+          var piv = exp(log(documentTopicDistribution(documentIdx,::))
+            + log(topicWordDistribution(::, documentTokenDenseMatrix(documentIdx, wordIdx)).t ))
+          val pivNormalized = (piv / sum(piv)).t
+
+          z(documentIdx, wordIdx) = new Multinomial[DenseVector[Double], Int](pivNormalized).draw()
+        }
+      }
+
+      for (documentIdx <- 0 to nDoucments-1) {
+
+        var updateTopic = DenseVector.zeros[Double](nTopic)
+
+        for (topicIdx <- 0 to nTopic-1) {
+          var f = (x: Int) => x == topicIdx
+            updateTopic(topicIdx) = z(documentIdx,::).inner.findAll(f).size
+
+          documentTopicDistribution(documentIdx, ::) := new distributions.Dirichlet(
+            alpha * DenseVector.ones[Double](nTopic) + updateTopic).draw().t
+        }
+      }
+
+      for (topicIdx <- 0 to nTopic-1) {
+        var updateVocab = DenseVector.zeros[Double](nVocab)
+
+        for (vocabIdx <- 0 to nVocab-1) {
+          for (documentIdx <- 0 to nDoucments-1) {
+            for (vocabDualIdx <- 0 to nVocab-1) {
+              if (documentTokenDenseMatrix(documentIdx, vocabDualIdx) == vocabIdx &&
+              z(documentIdx, vocabDualIdx) == topicIdx) {
+                updateVocab(vocabIdx) += 1
+              }
+            }
+          }
+        }
+        topicWordDistribution(topicIdx, ::) := new distributions.Dirichlet(
+          gamma * DenseVector.ones[Double](nVocab) + updateVocab
+        ).draw().t
+      }
+
+
+    }
   }
 
 
